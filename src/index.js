a4868a2568a980c696b63fdae097866a3f50d1ac
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2020-04-05 17:40:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
-import Button from "./Button";
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button />
   </React.StrictMode>,
   rootElement
 );
