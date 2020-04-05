eca82b0934f4b325c31b0d00769090ab01c871b2
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2020-04-05 17:39:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import Button from "./Button.js";
+import Display from "./Display.js";
+
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <Button onClickFunction={incrementCounter} increment={5}/>
+      <Button onClickFunction={incrementCounter} increment={10}/>
+      <Button onClickFunction={incrementCounter} increment={100}/>
+      <Display message={counter} />
+    </>
   );
 }
